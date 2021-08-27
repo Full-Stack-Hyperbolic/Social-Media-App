741bdd33637cb2e65e3765c8df526843f1f701b4
Karsenh <KarsenHansen@gmail.com>
2021-08-26 19:31:09 -0700

    added likeCount & commentCount on server side to reduce front-end processing

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,13 @@ const usersResolvers = require('./users.js');
 const commentsResolvers = require('./comments.js');
 
 module.exports = {
+  Post: {
+    likeCount: parent => {
+      console.log(parent);
+      return parent.likes.length;
+    },
+    commentCount: parent => parent.comments.length,
+  },
   Query: {
     ...postsResolvers.Query,
     ...usersResolvers.Query,
