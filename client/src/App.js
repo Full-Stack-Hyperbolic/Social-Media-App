3dca618fbbf7c73ca625c33274525c19648b0e60
Karsenh <KarsenHansen@gmail.com>
2021-08-28 17:12:22 -0700

    added front-end / routing & semantic UI

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { Container } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import MenuBar from './Components/MenuBar';
+import Home from './Components/pages/Home';
+import Login from './Components/pages/Login';
+import Register from './Components/pages/Register';
+
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className='ui container'>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
